ce775c8495093899e50e88082c1a27b0b06dd75d
psehyun1075 <163107812+psehyun1075@users.noreply.github.com>
2025-03-26 14:11:50 +0000

    psehyun Week1

--- mbti_proj/app.py
+++ mbti_proj/app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html') 
 
 @app.route('/home')
 def home():
