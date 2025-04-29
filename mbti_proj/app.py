c346f1d843ef739ab35545d0ed3730b20112aa5a
psehyun1075 <163107812+psehyun1075@users.noreply.github.com>
2025-04-29 02:03:11 +0000

    4/29

--- mbti_proj/app.py
+++ mbti_proj/app.py
@@ -1,4 +1,15 @@
-from flask import Flask, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+import mysql.connector
+
+friend_bp = Blueprint('friend', __name__)
+db = mysql.connector.connect(
+    host="localhost", 
+    user="flaskuser",  # mysql 새사용자 생성->flaskuser
+    password="1234", 
+    database="mbti_db")
+
+cursor = db.cursor(dictionary=True) # 데이터 dict 형태로 전달
+CURRENT_USER_ID = 1
 
 app = Flask(__name__)
 
@@ -14,5 +25,8 @@ def home():
 def user_register():
     return render_template('user_register.html') 
 
+@app.route('/user_login')
+def user_login():
+    return render_template('user_login.html') 
 if __name__ == '__main__':
     app.run(debug = True)
