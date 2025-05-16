96ddf0a82e453247b9a28a2acb6f1ab02682d3bd
userbiin <162470759+userbiin@users.noreply.github.com>
2025-05-16 04:50:04 +0000

    main app blueprint 수정

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, session
 import sys, os
-from feed_routes import feed_bp
-from mbti_feature_routes import mbti_feature_bp
+from feature_feed.feed_routes import feed_bp
+from feature_feed.mbti_feature_routes import mbti_feature_bp
 
 app = Flask(__name__)
 app.register_blueprint(feed_bp)
@@ -12,9 +12,16 @@ app.register_blueprint(mbti_feature_bp)
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from feature_friend.friend import friend_bp
 from feature_login.login import user_app
+from mbti_friend_lookup.app import find
+from feature_feed.mbti_feature_routes import mbti_feature_bp
+from feature_feed.feed_routes import feed_bp
 
 app = Flask(__name__)
 app.register_blueprint(friend_bp)
+app.register_blueprint(user_app)
+app.register_blueprint(find)
+app.register_blueprint(mbti_feature_bp)
+app.register_blueprint(feed_bp)
 
 # 테스트용
 app.secret_key = 'any-random-string'  # 세션 사용을 위한 키 임시부여
@@ -26,7 +33,6 @@ def simulate_login():
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
